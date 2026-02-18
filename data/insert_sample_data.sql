a3896fa52854febea83917eb57e7da85699f8c2d
Abdullah <abdullah.alattwi@gmail.com>
2026-02-18 08:35:49 +0300

    Add sample data (DML)

---
+++ data/insert_sample_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO PATIENT VALUES
+(1, 'Ali Ahmed', '1995-05-10', 'Male', '0501234567', 'ali@gmail.com');
+
+INSERT INTO DOCTOR VALUES
+(10, 'Dr. Sara Saleh', 'Cardiology', '0559876543', '2020-01-15');
+
+INSERT INTO APPOINTMENT VALUES
+(100, 1, 10, '2025-01-10', '10:00:00', 'Completed');
+
+INSERT INTO MEDICAL_RECORD VALUES
+(500, 100, 'High blood pressure', 'Follow-up required', '2025-01-10');
+
+INSERT INTO PRESCRIPTION VALUES
+(900, 500, 'Amlodipine', '5mg', '30 days');
+
+INSERT INTO BILL VALUES
+(300, 100, 200.00, 50.00, 250.00, 'Paid');
